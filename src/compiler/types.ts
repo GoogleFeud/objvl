43d804544ebdc7eec35beb656c6982eb2de3035e
GoogleFeud <volen.sl666@gmail.com>
2022-01-26 20:35:53 +0200

    Schema types

---
+++ src/compiler/types.ts
@@ -0,0 +1,68 @@
+
+export type Schema = Omit<SchemaObjectType, "type">;
+
+export type ValidationError<T = Record<string, unknown>> = number | T & { code: number } | string;
+
+export interface SchemaStringType {
+    type: "string",
+    minLen?: number,
+    maxLen?: number,
+    pattern?: RegExp,
+    validator?: (value: string) => boolean;
+    errors?: {
+        minLen?: (value: string, min: number) => ValidationError,
+        maxLen?: (value: string, max: number) => ValidationError,
+        pattern?: (value: string, pattern: RegExp) => ValidationError,
+        validator?: (value: string) => ValidationError
+    }
+}
+
+export interface DefaultErrors {
+    missing?: () => ValidationError,
+    type?: (actual: unknown) => ValidationError
+}
+
+export interface SchemaIntType {
+    type: "number" | "i32" | "i16" | "i8" | "u32" | "u16" | "u8" | "float" | "f32",
+    min?: number,
+    max?: number,
+    validator?: (value: number) => boolean,
+    errors?: DefaultErrors & {
+        min?: (value: number, min: number) => ValidationError,
+        max?: (value: number, max: number) => ValidationError,
+        validator?: (value: string) => ValidationError
+    }
+}
+
+export interface SchemaArrayType {
+    type: "array",
+    minLen?: number,
+    maxLen?: number,
+    items?: SchemaType | Array<SchemaType>,
+    validator?: (value: Array<unknown>) => boolean,
+    errors?: DefaultErrors & {
+        minLen?: (value: number, min: number) => ValidationError,
+        maxLen?: (value: number, max: number) => ValidationError,
+        validator?: (value: string) => ValidationError
+    }
+}
+
+export interface SchemaBoolType {
+    type: "bool",
+    errors?: DefaultErrors
+}
+
+export interface SchemaObjectType {
+    type: "object"
+    properties: Record<string, SchemaType>,
+    required?: Array<string>,
+    allowExtra?: boolean,
+    validator?: (value: Record<string, unknown>) => boolean,
+    errors?: DefaultErrors & {
+        required?: (value: number, min: number) => ValidationError,
+        allowExtra?: () => ValidationError,
+        validator?: (value: Record<string, unknown>) => ValidationError
+    }
+}
+
+export type SchemaType = SchemaStringType | SchemaIntType | SchemaArrayType | SchemaObjectType | SchemaBoolType;
--- test/inline.test.ts
+++ test/inline.test.ts
@@ -1,17 +1,20 @@
 
 import { inlineFunc } from "../src/compiler/inliner";
 
-test("Inline Function: simple inlining", () => {
+test("Simple inlining", () => {
     expect(inlineFunc((a: number, b: number) => a + b, ["5", "5"])).toBe("5+5");
 });
 
-test("Inline Function: complex inlining", () => {
+test("Complex inlining", () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect(inlineFunc((a: any, b: any) => a.edit(b).parse("a").parse("b"), ["c", "d"])).toBe("c.edit(d).parse(\"a\").parse(\"b\")");
 });
 
-test("Inline Function: failed inlining", () => {
+test("Failed inlining", () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect(inlineFunc((a: any, b: any) => {console.log(a + b); }, ["c", "d"])).toBe("((a, b) => { console.log(a + b); } )(c,d)");
 });
 
+test("Zero param inlining", () => {
+    expect(inlineFunc(() => 505, [])).toBe("505");
+});
